1a59d9980acb30863aa5255b40569eab21d22f7c
Snatch-jie <289621851@qq.com>
2016-05-10 21:01:15 +0800

    2

---
+++ js/rem.js
@@ -0,0 +1,18 @@
+(function (doc,win) {
+	var docEl = doc.documentElement,
+	resizeEvt = 'orientationchange' in window ? 'orientationchange':'resize',
+	recalc = function(){
+		var clientWidth = docEl.clientWidth;
+		if(!clientWidth) return;
+		if(clientWidth >= 640) {
+			docEl.STYLE_RULE.fontSize ='100px';
+		}else {
+			docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+		}
+	};
+	if(!doc.addEventListener) return;
+	win.addEventListener(resizeEvt,recalc,false);
+	doc.addEventListener('DOMContentLoaded',recalc,false);
+
+	
+})(document,window);
